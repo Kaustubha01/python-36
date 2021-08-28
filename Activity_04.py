7b8b6d1902d1ef92ca3ade9240eb5fd8e6e39d57
Kaustubha01 <89680313+Kaustubha01@users.noreply.github.com>
2021-08-28 14:43:25 +0530

    Update Activity_04.py

---
+++ Activity_04.py
@@ -0,0 +1,6 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=a+b
+print("%d+%d=%d\n"%(a,b,c))
