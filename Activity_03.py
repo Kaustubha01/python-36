947be829e2959ebda7cba4d31eba807ed349ab2e
Kaustubha01 <89680313+Kaustubha01@users.noreply.github.com>
2021-08-28 14:23:13 +0530

    Update Activity_03.py

---
+++ Activity_03.py
@@ -0,0 +1,7 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+str1=input("Enter string 1")
+str2=input("Enter string 2")
+str3= str1 + str2
+print("concatenated string=",str3)
+print("repeated string =",str3+str3+str3+str3+str3)
