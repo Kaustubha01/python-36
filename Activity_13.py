fccad5eab4a6e482c3cc53b6157608e85f82b933
Kaustubha01 <89680313+Kaustubha01@users.noreply.github.com>
2021-08-29 17:29:49 +0530

    Update Activity_13.py

---
+++ Activity_13.py
@@ -0,0 +1,6 @@
+start = int(input ("Enter starting number : ")) 
+end = int(input ("Enter ending number: "))
+
+for num in range(start, end + 1):
+     if num % 2 != 0:
+        print(num, end = " ")
