ee186a685ef82c23032276667f0dc397e0afd894
Kaustubha01 <89680313+Kaustubha01@users.noreply.github.com>
2021-08-29 13:32:38 +0530

    Update Activity_09.py

---
+++ Activity_09.py
@@ -0,0 +1,9 @@
+l=float(input("Enter the lenght"))
+h=float(input("Enter the height"))
+b=float(input("Enter the breadth"))
+
+k=float((l**2+b**2+h**2)**0.5)
+v=float((b**2*h**2)/k)
+ print("volume of tromboloid",format(v,".3f"))
+r=(3*v/(4*3.1459))**(1/3)
+ print("radius",format(r,".3f"))
