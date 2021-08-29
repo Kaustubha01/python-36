f34c113b9975d64aa0beaddd30d0107b597c9183
Kaustubha01 <89680313+Kaustubha01@users.noreply.github.com>
2021-08-29 21:39:10 +0530

    Update Activity_11.py

--- Activity_11.py
+++ Activity_11.py
@@ -1,16 +1,16 @@
 def input_number(): 
-number1= int(input("Enter the first number"))
-number2= int(input("Enter the second number"))
+ number1= int(input("Enter the first number"))
+ number2= int(input("Enter the second number"))
 return number1, number2
 
 def add(a,b):
-return(a+b)
+ return(a+b)
 
 def display(a, b, c):
-print(f”{a} + {b} = {c}”)
+ print(f”{a} + {b} = {c}”)
 
 def main():
-number1, number2 = input_number()
-answer= add(number1,number2)
-display(number1,number2,answer)
+ number1, number2 = input_number()
+ answer= add(number1,number2)
+  display(number1,number2,answer)
 main()
